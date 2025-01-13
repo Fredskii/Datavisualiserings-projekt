06b2386b21eef615d8b892846487b79b08829e43
Fredskii <Frederikolesen@hotmail.com>
2025-01-13 13:58:33 +0100

    commit

--- Koden.js
+++ Koden.js
@@ -1,8 +1,6 @@
 function setup() {
   createCanvas(400, 400);
   background(250);
-let Button = createButton("Klik for crypto");
-Button.position(250,250);
-Button.size(150,120);
-Button.mousePressed(Crypto);
+  let button = createButton('click me');
+  button.position(250, 250);
 }
