4b66464ae44d8829bbee67e1724415c1adbd6faa
Fredskii <Frederikolesen@hotmail.com>
2025-01-27 14:12:51 +0100

    fixed the merge error

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
-let Scrollbar;
-let canvasHøjde = 1700;
-let canvasBredde = 780
-let api;
-
-const valutaer = []
-
 function setup() {
-  createCanvas(canvasHøjde, canvasBredde);
+  createCanvas(1700, 780);
   background(400);
-  Scrollbar = createSelect();
+  let Scrollbar = createSelect();
   Scrollbar.position(0,0);
   Scrollbar.size(120,75)
   for (let i = 0; i<valutaer.length; i++){
@@ -30,23 +23,60 @@ loadData();
 function loadData(){
   loadJSON('https://api.coinlore.net/api/tickers/',udskrivData);
 }
-
-
 function udskrivData(data){
-  api = data;
-  console.log(api.data);
+  console.log(data);
+}
+function draw(){
+
+}
+let Krypto;
+let DropMenu;
+let i;
+let KryptoValuta;
+let IndexTal;
+let KryptoValg = null;
+
+function setup() {
+  createCanvas(600, 400);
+
+  //API fra CoinLore med et datasæt, der er indsat med loadJSON.
+  loadJSON("https://api.coinlore.net/api/tickers/", (data) => {
+    Krypto = data.data;
+
+    //Dropmenu opsat, og et startervalg.
+    DropMenu = createSelect();
+    DropMenu.position(10, 10);
+    DropMenu.size(200, 20);
+    DropMenu.option("Vælg en Kryptovaluta");
+
+    //For-lykke af hele datasættet af kryptovalutaer, som bliver til valgmuligheder i dropmenuen.
+    for (i = 0; i < Krypto.length; i++) {
+      KryptoValuta = Krypto[i];
+      DropMenu.option(`${KryptoValuta.symbol}: ${KryptoValuta.name}`, i);
+    }
+
+    //En A-Sync funktion der gør at når man vælger i dropmenuen, viser den dens data ved brug af datasættets array.
+    DropMenu.changed(() => {
+      IndexTal = DropMenu.value();
+      KryptoValg = Krypto[IndexTal];
+    });
+  });
 }
 
-function draw(){
-  clear();
-  if(api != null && Scrollbar.value() != null){
-    text(api.data[Scrollbar.value()].name,100,100)
-    text(api.data[Scrollbar.value()].rank,200,100)
-    text(api.data[Scrollbar.value()].symbol,300,100)
-    text(api.data[Scrollbar.value()].price_usd,400,100)
-    text(api.data[Scrollbar.value()].percent_change_1h,100,200)
-    text(api.data[Scrollbar.value()].percent_change_24h,200,200)
-    text(api.data[Scrollbar.value()].percent_change_7d,300,200)
-  }
+function draw() {
+  background(250);
+  textSize(16);
+  text("Vælg den kryptovaluta du søger ved at klikke på dropmenuen", 10, 50);
 
+  // Dataen udsendes i tekst, af den kryptovaluta valgt i dropmenuen.
+  if (KryptoValg) {
+    text(`Navn: ${KryptoValg.name}`, 10, 100);
+    text(`Forkortelse: ${KryptoValg.symbol}`, 10, 120);
+    text(`Pris (USD): ${KryptoValg.price_usd}$`, 10, 140);
+    text(`Markedsværdi (USD): ${KryptoValg.market_cap_usd}$`, 10, 160);
+    text(`1-times ændring: ${KryptoValg.percent_change_1h}%`, 10, 180);
+    text(`24-timers ændring: ${KryptoValg.percent_change_24h}%`, 10, 200);
+    text(`7-dages ændring: ${KryptoValg.percent_change_7d}%`, 10, 220);
+    text(`Pris i Bitcoin: ${KryptoValg.price_btc} BTC`, 10, 240);
+  }
 }
